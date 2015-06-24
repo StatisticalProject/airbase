58b5b1b5de52f0d0c4c347340d95cc09fe213218
Sebastien Diaz <sebastien.diaz@misys.com>
2015-06-24 18:19:58 +0200

    add integration

---
+++ import.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#sudo apt-get install python-pip python-dev build-essential
+#$ sudo pip install --upgrade pip
+#$ sudo pip install --upgrade virtualenv
+# sudo pip install rethinkdb
+rethinkdb import -f data/CH_meta.xml.json --table test.users
---
+++ programme.js
@@ -0,0 +1,38 @@
+r.tableDrop('countries');
+r.tableDrop('stations');
+r.tableDrop('measures');
+
+
+r.tableCreate('countries');
+r.tableCreate('stations');
+r.tableCreate('measures');
+
+var countries=r.table('users').map(function ( doc) {
+  return {id: doc('airbase').getField('country').getField('country_iso_code'),country:doc('airbase').getField('country').without('station')};
+});
+
+
+var stations=r.table('users').concatMap(function ( doc) {
+               return doc('airbase')('country')('station').without('measurement_configuration').map(function(station) {
+                 return station.merge({id:station('@Id')});
+                                                                                                    });
+             });
+
+var measures=r.table('users').concatMap(function ( doc) {
+  return doc('airbase')('country')('station').map(function(station) {
+    return {id:station('@Id'),mesure:station('measurement_configuration')};
+});
+});
+
+r.table('countries').insert(countries,{conflict:'replace'});
+r.table('stations').insert(stations,{conflict:'replace'});
+r.table('measures').insert(measures,{conflict:'replace'});
+
+
+r.table('users').map(function ( doc) {
+  return {station: doc('airbase').getField('country').getField('station').without('measurement_configuration')};
+})
+
+r.table('users').concatMap(function ( doc) {
+  return doc('airbase')('country')('station')('measurement_configuration');
+})
